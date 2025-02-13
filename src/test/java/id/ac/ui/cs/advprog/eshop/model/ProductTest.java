a0999b30a789a2cdd806ce37fe5d36ccbb3c4933
patriciatyas <patriciatyas11@gmail.com>
2025-02-14 00:51:43 +0700

    add spacing on code lines for cleaner code

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class ProductTest {
     Product product;
+
     @BeforeEach
     void setUp () {
         this.product = new Product();
@@ -23,6 +25,7 @@ class ProductTest {
     void testGetProductName() {
         assertEquals("Sampo Cap Bambang", this.product.getProductName());
     }
+
     @Test
     void testGetProductQuantity() {
         assertEquals(100, this.product.getProductQuantity());
