baed80cb45b4d907469afe4d8712db37128a8dba
patriciatyas <patriciatyas11@gmail.com>
2025-03-06 22:21:19 +0700

    [GREEN] Implement OrderRepository class

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -132,7 +132,7 @@ class OrderServiceImplTest {
     @Test
     void testFindAllByAuthorIfAllLowercase() {
         Order order = orders.get(1);
-        doReturn(orders).when(orderRepository).findAllByAuthor(order.getAuthor().toLowerCase());
+        doReturn(new ArrayList<Order>()).when(orderRepository).findAllByAuthor(order.getAuthor().toLowerCase());
 
         List <Order> results = orderService.findAllByAuthor(order.getAuthor().toLowerCase());
         assertTrue(results.isEmpty());
